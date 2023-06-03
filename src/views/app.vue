f2f888d0f2aeaa1591d3755e984b569f32bfb0a5
suki <suki.music2004@gmail.com>
2023-06-03 19:38:02 +0900

    多少の変更(頑張った)

--- src/views/app.vue
+++ src/views/app.vue
@@ -1,7 +1,7 @@
 <script setup>
-import Head_Bar from './../components/header.vue'
+import Head_Bar from '../components/head_Bar.vue'
 import Popup from './../components/popup_add.vue'
-import List from './../components/list.vue'
+import List from '../components/Main_list.vue'
 </script>
 
 <template>
