2e8aa340f58123799ee582691f7438c2f5400fc4
suki <suki.music2004@gmail.com>
2023-06-03 19:01:19 +0900

    Popupを作った

---
+++ src/components/list.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <p></p>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/app.vue
+++ src/views/app.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Head_Bar from './../components/header.vue'
 import Popup from './../components/popup_add.vue'
+import List from './../components/list.vue'
 </script>
 
 <template>
   <Head_Bar/>
-  <Popup/>
+  <List />
 </template>
 
 <style scoped>
