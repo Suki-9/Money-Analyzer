d861ff101b706dfafd360e159d3978ea02f3034a
suki <suki.music2004@gmail.com>
2023-06-25 03:08:33 +0900

    書き直した

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return{
+      変数名: 初期値
+    }
+  },
+  mounted(){
+    axios
+      .get('URL')
+      .then(response => (this.変数名 = response))
+  }
+}
+</script>
+
+<template>
+
+</template>
