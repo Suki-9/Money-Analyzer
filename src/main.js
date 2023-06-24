9a95fd454147662c87ae68578deb9113c3ef9e97
suki <suki.music2004@gmail.com>
2023-06-25 02:40:22 +0900

    全部消した

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './assets/css/main.css'
-import App from './App.vue'
-import Head from './components/HeadBar.vue'
-import router from './router'
-
-createApp(Head).mount('#head')
-createApp(App).use(router).mount('#app')
